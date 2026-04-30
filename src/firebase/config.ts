4051b3ab90e09e9fe2f93c7630374b8faff4f8f3
Obuleswar <obuleswaror@gmail.com>
2026-04-30 07:18:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1630330973-8fc61",
+  "appId": "1:475617687867:web:bb207db2b5dfe08e0e1f1e",
+  "apiKey": "AIzaSyAq98FocBNP8c04jCLUZ79amE-nHNIuASI",
+  "authDomain": "studio-1630330973-8fc61.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "475617687867"
+};
