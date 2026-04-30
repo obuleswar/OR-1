60666d9ea8d8ca320ff6b31dff35d30a6e19d3b8
Obuleswar <obuleswaror@gmail.com>
2026-04-30 15:32:11 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAq98FocBNP8c04jCLUZ79amE-nHNIuASI",
-  authDomain: "studio-1630330973-8fc61.firebaseapp.com",
-  databaseURL: "https://studio-1630330973-8fc61-default-rtdb.firebaseio.com",
-  projectId: "studio-1630330973-8fc61",
-  storageBucket: "studio-1630330973-8fc61.firebasestorage.app",
-  messagingSenderId: "475617687867",
-  appId: "1:475617687867:web:bb207db2b5dfe08e0e1f1e"
+  "projectId": "studio-1630330973-8fc61",
+  "appId": "1:475617687867:web:bb207db2b5dfe08e0e1f1e",
+  "apiKey": "AIzaSyAq98FocBNP8c04jCLUZ79amE-nHNIuASI",
+  "authDomain": "studio-1630330973-8fc61.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "475617687867"
 };
